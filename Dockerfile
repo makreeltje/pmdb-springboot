16387e9188039da76ba9fd05ba0cd1a0e6edb61f
Rick Meels <rick.meels@gmail.com>
2020-05-24 11:38:10 +0200

    Change in pom and dockerfile for CD docker hub

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM openjdk:8-jdk-alpine
-ARG JAR_FILE=target/*.jar
-COPY ${JAR_FILE} pmdb-0.1.0.jar
-ENTRYPOINT ["java","-jar","/pmdb-0.1.0.jar"]
+FROM java:8
+EXPOSE 8080
+ADD target/pmdb-springboot.jar pmdb-springboot.jar
+ENTRYPOINT ["java","-jar","pmdb-springboot.jar"]
--- src/main/java/com/pmdb/api/ApiApplication.java
+++ src/main/java/com/pmdb/api/ApiApplication.java
@@ -2,10 +2,18 @@ package com.pmdb.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ApiApplication {
+
+	@GetMapping("/")
+	public String home() {
+		return "Hello World from pmdb";
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
